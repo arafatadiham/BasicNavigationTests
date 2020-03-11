2181fdbe7c7f5620e9b0f67e93ae41cb65deb9ec
arafatadiham <arafat.adiham@gmail.com>
2020-03-10 22:21:36 -0700

    HomeWork

---
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -0,0 +1,17 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class StringUtility {
+
+    public static void verifyEquals (String expected, String actual){
+        if(expected.equals(actual)){
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+        System.out.println(expected);
+        System.out.println(actual);
+    }
+
+}
